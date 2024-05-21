6ffb1f97e68a31cab3f7b9e66777c061d864d749
JacaMan <Pd@outlook.com>
2024-05-21 10:06:11 -0300

    adicionei delete

--- src/controllers/CarroController.js
+++ src/controllers/CarroController.js
@@ -15,10 +15,44 @@ module.exports = {
         }
 
         res.json(json);
+    },
+
+    buscarUm: async (req, res) =>{
+        let json = {error:'', result:[]};
+
+        let codigo = req.params.codigo;//para pegar o parametro na requisicao
+        let carro = await CarroService.buscarUm(codigo);
+
+        if(carro){
+            json.result = carro;
+        }
+
+        res.json(json);
+    },
+
+    inserir: async (req, res) =>{
+        let json = {error:'', result:[]};
+
+        let modelo = req.body.modelo;
+        let placa = req.body.placa;
+
+        if(modelo && placa){
+            let CarroCodigo = await CarroService.inserir(modelo, placa);
+            json.result = {
+                codigo: CarroCodigo,
+                modelo,
+                placa
+            };
+        }else{
+            json.error = 'Campos não enviados';
+        }
+        res.json(json);
+    },
+    excluir: async (req, res) =>{
+        let json = {error:'', result:[]};
+
+        await CarroService.excluir(req.params.codigo);
+
+        res.json(json);
     }
-
-
-
-
-
-}
+    }    
